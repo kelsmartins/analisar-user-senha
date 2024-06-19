0f69742da3da199aa935fb2b9842bc824b5697c8
Kelso Martins <kelsoglauber11@gmail.com>
2024-06-19 16:51:35 -0400

    fiz o codigo JS

---
+++ projetos/projTODOLIST/script.js
@@ -0,0 +1,43 @@
+const btTask = document.getElementById('btnTask');
+
+const values = [];
+
+function inpTask(){
+    let getinpTask = document.getElementById('inputTask');
+    return getinpTask;
+}
+
+function theList(){
+    let gettheList = document.getElementById('list');
+    return gettheList;
+}
+
+
+function seila(){
+    window.alert('oi')
+}
+
+function addTask(){
+
+    let liTask = inpTask();
+
+    let newLi = document.createElement('li');
+    newLi.innerHTML = `<button id="btn-left" onclick="didTask"></button>${liTask.value}<button id="btn-right" onclick="eraseTask()"></button>`;
+
+    values.push(newLi)  
+    liTask.value = '';
+    
+    showInlist()
+}
+
+function showInlist(){
+
+    let list = theList();
+
+    for(let v in values){
+        list.appendChild(values[v])
+    }
+}
+
+
+btTask.addEventListener('click', addTask)
